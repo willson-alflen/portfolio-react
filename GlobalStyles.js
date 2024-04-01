7e0cea0bfffbec3bda77f272dac21d79cb23199f
willson-alflen <willson.alflen@gmail.com>
2024-04-01 15:35:28 -0300

    project setup

---
+++ GlobalStyles.js
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html, body {
+    background-color: #fff;
+    font-family: "Lato", sans-serif;
+  }
+
+  body::-webkit-scrollbar {
+    display: none;
+  }
+
+  body {
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    border: none;
+    background: none;
+    cursor: pointer;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`
+
+export default GlobalStyles
